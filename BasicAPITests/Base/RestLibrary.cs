2997247372dae02b8d3e65047fac267a4a635f40
Vaibhav Ranjan <ranjanvaibhav8@gmail.com>
2026-07-13 13:03:38 +0530

    refactoring to the tests by Vaibhav

---
+++ BasicAPITests/Base/RestLibrary.cs
@@ -0,0 +1,26 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicAPITests.Base
+{
+    public class RestLibrary
+    {
+        public RestClient restClient { get; }
+
+        public RestLibrary()
+        {
+            var restClientOptions = new RestClientOptions
+            {
+                BaseUrl = new Uri("https://localhost:5001/"),
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+
+            };
+
+            restClient = new RestClient(restClientOptions);
+        }
+    }
+}
